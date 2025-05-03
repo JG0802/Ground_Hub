5452b0d8b29cb244ea357eceb0f6a0a6631418f9
이재균 <jamesgyun@gmail.com>
2025-05-03 14:55:59 +0900

    백에 메이븐 생성

---
+++ back/gh_back/src/test/java/com/ce/gh_back/GhBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ce.gh_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
